19f2b4ca16d09cc0c86408aa57eab3f2b2bb9f72
TJ <cooploops@gmail.com>
2017-10-01 14:43:05 -0700

    add basic form and firebase init

---
+++ javascript/sample.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDqeua3U0m4B5QtbGZod07AsNxK35zHQ0U",
+    authDomain: "my-awesome-project-c1de8.firebaseapp.com",
+    databaseURL: "https://my-awesome-project-c1de8.firebaseio.com",
+    projectId: "my-awesome-project-c1de8",
+    storageBucket: "my-awesome-project-c1de8.appspot.com",
+    messagingSenderId: "968353723398"
+  };
+  firebase.initializeApp(config);
