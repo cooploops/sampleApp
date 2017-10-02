96dda3b3b2237fa6cba76de0999d41240da5933d
TJ <cooploops@gmail.com>
2017-10-01 19:02:25 -0700

    follow example github fb login

--- javascript/sample.js
+++ javascript/sample.js
@@ -9,27 +9,60 @@
   };
   firebase.initializeApp(config);
 
-  var provider = new firebase.auth.FacebookAuthProvider();
+  function toggleSignIn () {
+    if(!firebase.auth().currentUser) {
+        firebase.auth().useDeviceLanguage();
+        // use default langauge settings of device being used
+        var provider = new firebase.auth.FacebookAuthProvider();
 
-  // use default langauge settings of device being used
-  firebase.auth().useDeviceLanguage();
+        provider.addScope('public_profile, user_birthday, user_friends');
+        
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        console.log(user);
+        // ...
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+        });
+    } else {
+      firebase.auth().signOut();
+    }
+    $("#loginButton").addClass('disabled')
+  }
+
+  function initApp () {
+    firebase.auth().onAuthStateChanged(function(user){
+      if (user) {
+        var displayName = user.displayName;
+        var email = user.email;
+        var emailVerified = user.emailVerified;
+        var photoURL = user.photoURL;
+        var isAnonymous = user.isAnonymous;
+        var uid = user.uid;
+        var providerData = user.providerData;
+
+        $("#userAccountDetails").text(JSON.stringify(user,null, ' '));
+      } else {
+        $("#userAccountDetails").text("null");
+      }
+      $("#loginButton").removeClass('disabled');
+    });
+    $("#loginButton").on("click", toggleSignIn);
+  }
+
+
+
   
-  windown.onload = function(){
-  firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
-  console.log(user);
-  // ...
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
+  window.onload = function(){
+    initApp();
 }
